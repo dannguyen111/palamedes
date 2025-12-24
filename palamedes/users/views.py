847ec072bc852a81fc9df49f0dcc19cc1c26c61a
Dan Nguyen <112693768+dannguyen111@users.noreply.github.com>
2025-12-24 15:16:07 -0800

    added user registration view

--- palamedes/users/views.py
+++ palamedes/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect to a success page or log in the user
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
